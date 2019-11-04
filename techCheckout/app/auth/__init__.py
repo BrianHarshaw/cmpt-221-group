dbcf320bf7f8fd4494cb7ed28b37c64e002d2e5d
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-11-03 19:58:06 -0500

    Init for files under auth

---
+++ techCheckout/app/auth/__init__.py
@@ -0,0 +1,7 @@
+#Base code taken from https://github.com/Dr-Crow/cmpt-221-examples/tree/master/Examples/example_of_new_layout
+#and then altered to match our needs.
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+from . import views
