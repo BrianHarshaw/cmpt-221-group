ba29793c05ddc3105527fe423d7df6b6cc7b4cc7
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-11-03 19:39:23 -0500

    Added comments

--- techCheckout/app/main/__init__.py
+++ techCheckout/app/main/__init__.py
@@ -1,3 +1,5 @@
+#Base code taken from https://github.com/Dr-Crow/cmpt-221-examples/tree/master/Examples/example_of_new_layout
+#and then altered to match our needs.
 from flask import Blueprint
 
 # Creates a blueprint for all our code under main
