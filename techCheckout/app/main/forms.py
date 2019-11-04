bc12e6418fe683f19f6e9420cce195f6ae595c8e
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-11-03 19:39:35 -0500

    Added comments

--- techCheckout/app/main/forms.py
+++ techCheckout/app/main/forms.py
@@ -1,3 +1,5 @@
+#Base code taken from https://github.com/Dr-Crow/cmpt-221-examples/tree/master/Examples/example_of_new_layout
+#and then altered to match our needs.
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email, Optional
