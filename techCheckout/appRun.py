46d6b03663b221f94217365886a141ef91741e03
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-11-04 17:26:35 -0500

    Added shell statement

--- techCheckout/appRun.py
+++ techCheckout/appRun.py
@@ -1,10 +1,16 @@
 #Base code taken from https://github.com/Dr-Crow/cmpt-221-examples/tree/master/Examples/example_of_new_layout
 #and then altered to match our needs.
 import os
-from app import create_app
+from app import create_app, db
+from app.models import User, Role
 
 # Creates an app using our function create_app. Also passes in default for config is nothing is set
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+# Allows us to run Flask via Flask Shell. Make sure to export/set FLASK_APP="flask_run.py"
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 # Run Flask Programmatically
 if __name__ == "__main__":
