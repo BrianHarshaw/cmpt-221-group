159b1f121e662b1c687abf057e43e14101f08d88
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-11-03 19:38:35 -0500

    Added comments

--- techCheckout/app/__init__.py
+++ techCheckout/app/__init__.py
@@ -1,4 +1,5 @@
-#Code taken from https://github.com/Dr-Crow/cmpt-221-examples/tree/master/Examples/example_of_new_layout
+#Base code taken from https://github.com/Dr-Crow/cmpt-221-examples/tree/master/Examples/example_of_new_layout
+#and then altered to match our needs.
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config
