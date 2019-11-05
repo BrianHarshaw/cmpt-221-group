e248377496a124bdbf2dd210e0f2ddb5e965d41b
BrianHarshaw <42786241+BrianHarshaw@users.noreply.github.com>
2019-11-04 20:26:45 -0500

    Fixed for Pg Admin
    I fixed a bit of syntax and issues that were preventing me from using this code to make a database on pg admin.

--- SQL/test1.sql
+++ SQL/test1.sql
@@ -10,6 +10,13 @@ CREATE TABLE IF NOT EXISTS User_T (
   	PRIMARY KEY (userID)
 );
 
+CREATE TABLE IF NOT EXISTS Department_T (
+	deptID								SERIAL				NOT NULL	UNIQUE,
+	locationID							TEXT				NOT NULL,
+	budgetID							SERIAL				NOT NULL,
+	PRIMARY KEY (deptID)
+);
+
 CREATE TABLE IF NOT EXISTS Asset_T (
   	assetID								SERIAL				NOT NULL	UNIQUE,
   	userID								SERIAL				NOT NULL,
@@ -18,16 +25,10 @@ CREATE TABLE IF NOT EXISTS Asset_T (
   	asset_name							TEXT				NOT NULL,
   	deptID								SERIAL				NOT NULL	UNIQUE,
   	PRIMARY KEY (assetID),
-  	CONSTRAINT Asset_T FOREIGN KEY (userID) REFERENCES User_T (userID),
-	CONSTRAINT Asset_T 	FOREIGN KEY (deptID) REFERENCES Department_T (deptID)
+  	FOREIGN KEY (userID) REFERENCES User_T (userID),
+	FOREIGN KEY (deptID) REFERENCES Department_T (deptID)
 );
 
-CREATE TABLE IF NOT EXISTS Department_T (
-	deptID								SERIAL				NOT NULL	UNIQUE,
-	locationID							TEXT				NOT NULL,
-	budgetID							SERIAL				NOT NULL,
-	PRIMARY KEY (deptID)
-);
 
 CREATE TABLE IF NOT EXISTS Permission_T (
 	userID								SERIAL				NOT NULL	UNIQUE,
@@ -35,5 +36,5 @@ CREATE TABLE IF NOT EXISTS Permission_T (
 	firstName							TEXT				NOT NULL,
 	lastName							TEXT				NOT NULL,
 	PRIMARY KEY (roleID),
-	CONSTRAINT Permission_T FOREIGN KEY (userID) REFERENCES User_T (userID)
+	FOREIGN KEY (userID) REFERENCES User_T (userID)
 );
