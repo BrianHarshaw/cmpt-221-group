e820e8344f5fb71da4cebd50e4b77fd2c038f419
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-11-11 19:39:09 -0500

    Changed the naming scheme since I was getting errors while trying to run with the database

--- techCheckout/migrations/test1.sql
+++ techCheckout/migrations/test1.sql
@@ -2,48 +2,48 @@ DROP TABLE IF EXISTS Roles;
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE IF NOT EXISTS Role_T (
-    roleID          SERIAL          NOT NULL        UNIQUE,
-    roleName        TEXT            NOT NULL,
-    PRIMARY KEY(roleID)
+    role_id         SERIAL          NOT NULL        UNIQUE,
+    role_name        TEXT            NOT NULL,
+    PRIMARY KEY(role_id)
 );
 
 CREATE TABLE IF NOT EXISTS Department_T (
-	departmentID						SERIAL				NOT NULL	UNIQUE,
-	locationID							TEXT				NOT NULL,
-	budgetID							SERIAL				NOT NULL,
-	PRIMARY KEY (departmentID)
+	department_id						SERIAL				NOT NULL	UNIQUE,
+	location_id							TEXT				NOT NULL,
+	budget_id							SERIAL				NOT NULL,
+	PRIMARY KEY (department_id)
 );
 
 CREATE TABLE IF NOT EXISTS User_T (
-  	userID 								SERIAL		  		NOT NULL	UNIQUE,
+  	user_id 								SERIAL		  		NOT NULL	UNIQUE,
   	email                   			TEXT        		NOT NULL,
-  	passwordHash                        TEXT                NOT NULL,
-  	firstName 							TEXT 				NOT NULL,
-  	lastName 							TEXT 				NOT NULL,
-  	phoneNumber                         BIGINT,
-  	roleID                              INT                 NOT NULL,
-  	PRIMARY KEY (userID),
-  	FOREIGN KEY (roleID) REFERENCES Role_T
+  	password_hash                        TEXT                NOT NULL,
+  	first_name 							TEXT 				NOT NULL,
+  	last_name 							TEXT 				NOT NULL,
+  	phone_number                         BIGINT,
+  	role_id                              INT                 NOT NULL,
+  	PRIMARY KEY (user_id),
+  	FOREIGN KEY (role_id) REFERENCES Role_T
 );
 
 CREATE TABLE IF NOT EXISTS Asset_T (
-  	assetID								SERIAL				NOT NULL	UNIQUE,
-  	userID								SERIAL				NOT NULL	UNIQUE,
+  	asset_id								SERIAL				NOT NULL	UNIQUE,
+  	user_id								SERIAL				NOT NULL	UNIQUE,
   	status								TEXT				NOT NULL,
   	model								TEXT				NOT NULL,
-  	assetName							TEXT				NOT NULL,
-  	deptID								SERIAL				NOT NULL	UNIQUE,
-  	PRIMARY KEY (assetID),
-  	FOREIGN KEY (userID) REFERENCES User_T
+  	asset_name							TEXT				NOT NULL,
+  	dept_id								SERIAL				NOT NULL	UNIQUE,
+  	PRIMARY KEY (asset_id),
+  	FOREIGN KEY (user_id) REFERENCES User_T
 );
 
 
 
 CREATE TABLE IF NOT EXISTS Permission_T (
-	userID								SERIAL				NOT NULL	UNIQUE,
-	roleID								TEXT				NOT NULL,
-	firstName							TEXT				NOT NULL,
-	lastName							TEXT				NOT NULL,
-	PRIMARY KEY (roleID),
-	FOREIGN KEY (userID) REFERENCES User_T
+	user_id								SERIAL				NOT NULL	UNIQUE,
+	role_id								TEXT				NOT NULL,
+	first_name							TEXT				NOT NULL,
+	last_name							TEXT				NOT NULL,
+	PRIMARY KEY (role_id),
+	FOREIGN KEY (user_id) REFERENCES User_T
 );
