11d69aa770aa2af28a612f6eb085df858d89f886
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-12-01 14:05:25 -0500

    Added more users for testing templates

--- techCheckout/migrations/test1.sql
+++ techCheckout/migrations/test1.sql
@@ -38,23 +38,28 @@ CREATE TABLE IF NOT EXISTS Asset_T (
   	FOREIGN KEY (user_id) REFERENCES User_T
 );
 
-INSERT INTO Role_T(role_name) VALUES ('Administrator');
-INSERT INTO Role_T(role_name) VALUES ('Student');
-INSERT INTO Role_T(role_name) VALUES ('Worker');
-INSERT INTO Role_T(role_name) VALUES ('Department Head');
-INSERT INTO Role_T(role_name) VALUES ('Faculty');
-
-INSERT INTO User_T(email, password_hash, first_name, last_name, phone_number, role_id) VALUES ('brian.harshaw1@marist.edu', 'pbkdf2:sha256:150000$EtOqtq89$7cd8d6bbfc4005c348242921ccf4509e457ef23e26e7a9832d15a151f24c6a9f', 'Brian', 'Harshaw', '8608051042', 1);
-
-/*
-Role table is taking the place of this for the time being
+/* Not necessary to use since you can just reference the roles table, but could be used
 CREATE TABLE IF NOT EXISTS Permission_T (
 	user_id								SERIAL				NOT NULL	UNIQUE,
 	role_id								TEXT				NOT NULL,
+	permission_level                    INT                 NOT NULL,
 	first_name							TEXT				NOT NULL,
 	last_name							TEXT				NOT NULL,
 	PRIMARY KEY (role_id),
 	FOREIGN KEY (user_id) REFERENCES User_T
 );
+*/
 
- */
+INSERT INTO Role_T(role_name) VALUES ('administrator');
+INSERT INTO Role_T(role_name) VALUES ('student');
+INSERT INTO Role_T(role_name) VALUES ('worker');
+INSERT INTO Role_T(role_name) VALUES ('department head');
+INSERT INTO Role_T(role_name) VALUES ('faculty');
+
+INSERT INTO User_T(email, password_hash, first_name, last_name, phone_number, role_id) VALUES ('brian.harshaw1@marist.edu', 'pbkdf2:sha256:150000$EtOqtq89$7cd8d6bbfc4005c348242921ccf4509e457ef23e26e7a9832d15a151f24c6a9f', 'Brian', 'Harshaw', '8608051042', 1);
+INSERT INTO User_T(email, password_hash, first_name, last_name, phone_number, role_id) VALUES ('brian46harshaw@gmail.com','pbkdf2:sha256:150000$h4r1xums$b32c1a11fcd435826bbae4dfd8e7ecaba1b2f3b5842dfe17f9299abd385f9664','Test', 'Student','1111111111',2);
+INSERT INTO User_T(email, password_hash, first_name, last_name, phone_number, role_id) VALUES ('brian2harshaw@gmail.com','pbkdf2:sha256:150000$h4r1xums$b32c1a11fcd435826bbae4dfd8e7ecaba1b2f3b5842dfe17f9299abd385f9664','Test', 'Faculty','1111111111',5);
+
+
+
+
