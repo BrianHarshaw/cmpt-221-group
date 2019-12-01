6813dc557a5f512c8424b3789c70ccfd4eb68153
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-12-01 15:14:34 -0500

    Changed the AddAsset route to include an engine that should add the data into the asset table

--- techCheckout/app/main/views.py
+++ techCheckout/app/main/views.py
@@ -1,10 +1,13 @@
 #Base code taken from https://github.com/Dr-Crow/cmpt-221-examples/tree/master/Examples/example_of_new_layout
 #and then altered to match our needs.
 from flask import render_template, redirect, flash
+from sqlalchemy import create_engine
+
 from .. import db
-from ..models import User
+from ..models import User, Asset
 from . import main
-from .forms import ContactForm
+from .forms import ContactForm, AddAsset
+
 
 
 # Allows us to declare our views outside the global scope.
@@ -21,6 +24,21 @@ def last_query(last_name):
             last_name=user.last_name,
             email=user.email), 200
 
+#Route that brings a verified user to the add asset form
+#Looked up that engines are a good way to add data to a database
+@main.route('/addNewAsset',methods=['GET','POST'])
+def addNewAsset():
+
+    form=AddAsset()
+    if form.validate_on_submit():
+        db_uri = 'sqlite:///db.sqlite'
+        engine = create_engine(db_uri)
+        ins = Asset.insert().values(
+            user_id=form.UID.data,status=form.status.data,
+            model=form.model.data,asset_name=form.asset_name.data,
+            department_id=form.DID.data)
+        engAdd = engine.connect()
+        engAdd.execute(ins)
 
 # redirect root route to login screen
 @main.route('/')
