b2e5df4ca9ee7312ee76ff34158d7a5b4964600c
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-12-01 14:22:16 -0500

    Fixed the department class and am adding department instances

--- techCheckout/migrations/test1.sql
+++ techCheckout/migrations/test1.sql
@@ -10,8 +10,8 @@ CREATE TABLE IF NOT EXISTS Role_T (
 
 CREATE TABLE IF NOT EXISTS Department_T (
 	department_id						SERIAL				NOT NULL	UNIQUE,
-	location_id							TEXT				NOT NULL,
-	budget_id							SERIAL				NOT NULL,
+	location						    TEXT				NOT NULL,
+	budget							    BIGINT			    NOT NULL,
 	PRIMARY KEY (department_id)
 );
 
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS User_T (
 );
 
 CREATE TABLE IF NOT EXISTS Asset_T (
-  	asset_id								SERIAL				NOT NULL	UNIQUE,
+  	asset_id							SERIAL				NOT NULL	UNIQUE,
   	user_id								SERIAL				NOT NULL	UNIQUE,
   	status								TEXT				NOT NULL,
   	model								TEXT				NOT NULL,
@@ -55,6 +55,8 @@ INSERT INTO Role_T(role_name) VALUES ('student');
 INSERT INTO Role_T(role_name) VALUES ('worker');
 INSERT INTO Role_T(role_name) VALUES ('department head');
 INSERT INTO Role_T(role_name) VALUES ('faculty');
+
+INSERT INTO Department_T()
 
 INSERT INTO User_T(email, password_hash, first_name, last_name, phone_number, role_id) VALUES ('brian.harshaw1@marist.edu', 'pbkdf2:sha256:150000$EtOqtq89$7cd8d6bbfc4005c348242921ccf4509e457ef23e26e7a9832d15a151f24c6a9f', 'Brian', 'Harshaw', '8608051042', 1);
 INSERT INTO User_T(email, password_hash, first_name, last_name, phone_number, role_id) VALUES ('brian46harshaw@gmail.com','pbkdf2:sha256:150000$h4r1xums$b32c1a11fcd435826bbae4dfd8e7ecaba1b2f3b5842dfe17f9299abd385f9664','Test', 'Student','1111111111',2);
