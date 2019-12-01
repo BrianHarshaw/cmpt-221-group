16d7e9b2bf17dded379fca1b7383dc8512629019
Brian Harshaw <Brian.Harshaw1@marist.edu>
2019-12-01 14:44:35 -0500

    Created an add asset form that can be used to make a new asset

--- techCheckout/app/main/forms.py
+++ techCheckout/app/main/forms.py
@@ -13,3 +13,12 @@ class ContactForm(FlaskForm):
     phone_number = StringField("Phone", validators=[Optional()])
     message = TextAreaField("Message", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+class AddAsset(FlaskForm):
+    UID = StringField("User ID", validators=[DataRequired()])
+    status = StringField("Status", validators=[DataRequired()])
+    model = StringField("Model", validators=[DataRequired()])
+    asset_name = StringField("Asset Name", validators=[DataRequired()])
+    DID = StringField("Department ID", validators=[DataRequired()])
+    submit = SubmitField("Submit")
